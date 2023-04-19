cd21f8dc9b19d14d577e8144512f77ccc0416d41
Nicola <shirnin.kolia@gmail.com>
2023-04-20 01:41:55 +0300

    получилась рекошитирующая траектория

--- main.py
+++ main.py
@@ -9,80 +9,75 @@ from scipy.integrate import odeint
 warnings.filterwarnings("ignore", category=MatplotlibDeprecationWarning)
 
 
-def ballistic_trajectory(x, t):
-    # TODO
-    pass
-
-def planning_trajectory(x, t):
-    # TODO
-    pass
-
-def ricocheting_trajectory(x, t):
-    # TODO
-    pass
-
-
-# The lift coefficient Cl is equal to the lift L divided by the quantity:
-# density r times half the velocity V squared times the reference area A.
-# Lift = CoefficientLift * rho * v^2 * (reference area / 2)
-def Lift(CoefficientLift, rho, v, L, d):
- return CoefficientLift * rho * v**2 * (L / 2)
-
-
-def temperature(h):
-    "Calculates air temperature [Celsius] at altitude [m]"
-    #from equations at
-    #   http://www.grc.nasa.gov/WWW/K-12/airplane/atmosmet.html
-    if h <= 11000:
-        #troposphere
-        t = 15.04 - .00649*h
-    elif h <= 25000:
-        #lower stratosphere
-        t = -56.46
-    elif h > 25000:
-        t = -131.21 + .00299*h
-    return t
-
-def pressure(h):
-    "Calculates air pressure [Pa] at altitude [m]"
-    # from equations at
-    #   http://www.grc.nasa.gov/WWW/K-12/airplane/atmosmet.html
-
-    t = temperature(h)
-
-    if h <= 11000:
-        # troposphere
-        p = 101.29 * ((t + 273.1) / 288.08) ** 5.256
-    elif h <= 25000:
-        # lower stratosphere
-        p = 22.65 * np.exp(1.73 - .000157 * h)
-    elif h > 25000:
-        p = 2.488 * ((t + 273.1) / 288.08) ** -11.388
-    return p
+# region data
+# def ballistic_trajectory(x, t):
+#     # TODO
+#     pass
+#
+# def planning_trajectory(x, t):
+#     # TODO
+#     pass
+#
+# def ricocheting_trajectory(x, t):
+#     # TODO
+#     pass
+#
+# def temperature(h):
+#     "Calculates air temperature [Celsius] at altitude [m]"
+#     #from equations at
+#     #   http://www.grc.nasa.gov/WWW/K-12/airplane/atmosmet.html
+#     if h <= 11000:
+#         #troposphere
+#         t = 15.04 - .00649*h
+#     elif h <= 25000:
+#         #lower stratosphere
+#         t = -56.46
+#     elif h > 25000:
+#         t = -131.21 + .00299*h
+#     return t
+#
+# def pressure(h):
+#     "Calculates air pressure [Pa] at altitude [m]"
+#     # from equations at
+#     #   http://www.grc.nasa.gov/WWW/K-12/airplane/atmosmet.html
+#
+#     t = temperature(h)
+#
+#     if h <= 11000:
+#         # troposphere
+#         p = 101.29 * ((t + 273.1) / 288.08) ** 5.256
+#     elif h <= 25000:
+#         # lower stratosphere
+#         p = 22.65 * np.exp(1.73 - .000157 * h)
+#     elif h > 25000:
+#         p = 2.488 * ((t + 273.1) / 288.08) ** -11.388
+#     return p
+# endregion
 
 # Constants
 g = 9.8066                  # gravitational acceleration, m/s^2
 R_earth = 6370000.0         # radius of the Earth, m
 
 # Initial condition of Hypersonic Aircraft
-T = 100000.0                # +initial thrust force, N
 m_fuel = 450.0              # +initial mass fuel of aircraft, kg
 m_ha = 1440 - m_fuel        # +initial mass of aircraft, kg
 m_total = m_ha + m_fuel     # total mass of aircraft, kg
 x = 0.0                     # initial Distance, m
-h = 2 * 1000.0              # 0-40.000 initial altitude, m
+y = 2 * 1000.0              # 0-40.000 initial altitude, m
 V = 3 * 340.0               # 2-6 +initial velocity, m/s    1 mach = 340 m/s
 alpha = np.radians(0)       # 0-10 +initial angle of attack, radians
-theta = np.radians(45)      # +initial angle of inclination of the flight trajectory, radians
+theta = np.radians(47)      # +initial angle of inclination of the flight trajectory, radians
 dt = 0.01                   # time step, s
-G_c = 0.02                  # +initial fuel burnout rate, N/s
+G_c = 0.001                  # +initial fuel burnout rate, N/s
 I_sp = 15.0                 # +initial specific impulse
 
 length = 12.5
 diameter = 0.5
-frontalArea = diameter**2/ (4 * np.pi)               # S = d**2 / (4 × π), где d — это диаметр
-CoefficientDrag = 0.4       # drag coefficient for cone 20 degrees
+frontalArea = diameter**2/ (4 * np.pi)
+lowerArea = length * diameter
 
+CoefficientDrag = 0.4       # drag coefficient for cone 20 degrees
+CoefficientLift = 1.2       # +lift coefficient for cone 20 degrees
 def rhoDensity(h):
     "Calculates air density at altitude"
     rho0 = 1.225 #[kg/m^3] air density at sea level
@@ -100,11 +95,11 @@ def rhoDensity(h):
     return rho
 
 
-# The drag coefficient Cd is equal to the drag D divided by the quantity:
-# density r times half the velocity V squared times the reference area A.
-# Drag = CoeffcientDrag * rho * v^2 * (reference area / 2)
-def DragX(CoefficientDrag, h, v, frontalArea):
+def Drag(CoefficientDrag, h, v, frontalArea):
     return CoefficientDrag * rhoDensity(h) * v**2 * (frontalArea / 2)
+
+def Lift(CoefficientLift, h, v, lowerArea):
+ return CoefficientLift * rhoDensity(h) * v**2 * (lowerArea / 2)
 
 def f(t):
     return G_c
@@ -118,15 +113,12 @@ def mass_after_fuel_burning(t, m_fuel):
 def thrust(m_fuel, g, I_sp):
     return m_fuel * g * I_sp # kg *m/s != кг*м / c^2
 
-def hypersonic_aircraft_model(y, t, R_earth, g, T, alpha, m_total, drag):
-    V, theta, x, h = y
+def hypersonic_aircraft_model(y, t, R_earth, g, T, alpha, m_total, drag, lift):
+    V, theta, x, y = y
 
-    # dV = (T * np.cos(alpha)) / m_total - g * np.sin(theta)
-    # dtheta = (T * np.sin(alpha)) / (m_total * V) - (g * np.cos(theta)) / V + (V * np.cos(theta)) / (R_earth + h)
-
-    dV = (T * np.cos(alpha)) / m_total - drag / m_total - g * np.sin(theta)
-    dtheta = (T * np.sin(alpha)) / (m_total * V) - (g * np.cos(theta)) / V + (V * np.cos(theta)) / (R_earth + h)
-    dx = V * np.cos(theta) * R_earth / (R_earth + h)
+    dV = (T * np.cos(alpha) - drag) / m_total - g * np.sin(theta)
+    dtheta = (T * np.sin(alpha) + lift) / (m_total * V)  - (g * np.cos(theta)) / V + (V * np.cos(theta)) / (R_earth + y)
+    dx = V * np.cos(theta) * R_earth / (R_earth + y)
     dy = V * np.sin(theta)
 
     return [dV, dtheta, dx, dy]
@@ -134,33 +126,48 @@ def hypersonic_aircraft_model(y, t, R_earth, g, T, alpha, m_total, drag):
 
 # Integration interval
 t = 0.0                     # initial time
-y0 = [V, theta, x, h]       # initial conditions
+y0 = [V, theta, x, y]       # initial conditions
 
 # Arrays to store results
 t_arr = [0.0]
-v_arr = [V / 340.0]
+v_arr = [V / 340]
 theta_arr = [np.degrees(theta)]
-x_arr = [x / 1000]
-h_arr = [h / 1000]
+x_arr = [x]
+y_arr = [y / 1000]
 m_arr = [m_total]
+thrust_arr = [0.0]
 
-while h > 0.0:
+is_Engine = True
 
-    if m_fuel > 0:
+while y > 0.0:
+
+    if theta < np.radians(19):
+        is_Engine = True
+    else:
+        is_Engine = False
+
+
+    if m_fuel > 0 and is_Engine == True:
+        alpha = np.radians(6)
         m_fuel = mass_after_fuel_burning(t, m_fuel)
         m_total = m_ha + m_fuel
         T = thrust(m_fuel, g, I_sp)
+    elif not is_Engine:
+        m_total = m_ha + m_fuel
+        T = 0
     else:
         m_total = m_ha
         T = 0
 
-    drag = DragX(CoefficientDrag, h, V, frontalArea)
-    # solve model
-    sol = odeint(hypersonic_aircraft_model, y0, [t, t + dt], args=(R_earth, g, T, alpha, m_total, drag))
+
+    drag = Drag(CoefficientDrag, y, V, frontalArea)
+    lift = Lift(CoefficientLift, y, V, frontalArea)
+
+    sol = odeint(hypersonic_aircraft_model, y0, [t, t + dt], args=(R_earth, g, T, alpha, m_total, drag, lift))
 
     # Update y0
     y0 = sol[-1]
-    V, theta, x, h = sol[-1]
+    V, theta, x, y = sol[-1]
 
     # Update time
     t += dt
@@ -170,29 +177,27 @@ while h > 0.0:
     v_arr.append(V / 340)
     theta_arr.append(np.degrees(theta))
     x_arr.append(x / 1000)
-    h_arr.append(h / 1000)
+    y_arr.append(y / 1000)
     m_arr.append(m_total)
+    thrust_arr.append(T)
 
 
 # region Output plots
-# Set the style for the plot
 sns.set_style("whitegrid")
 
-# Create a figure and axis object
 fig, ax1 = plt.subplots(figsize=(8, 8))
-
-# Plot the line
-sns.lineplot(x=x_arr, y=h_arr, ax=ax1, color='black', linewidth=2)
-
-# Set the axis labels and title
+sns.lineplot(x=x_arr, y=y_arr, ax=ax1, color='black', linewidth=2)
 ax1.set_xlabel('Дальность, км.')
 ax1.set_ylabel('Высота, км.')
 ax1.set_title('График зависимости дальности и высоты ЛА')
+ax1.set_xticks(np.linspace(np.min(x_arr), np.max(x_arr), 8))
+ax1.set_yticks(np.linspace(0, np.max(y_arr), 10))
 
-# Set the number of ticks on the x and y-axis
-ax1.set_xticks(np.linspace(x_arr[0], np.max(x_arr), 8))
-ax1.set_yticks(np.linspace(h_arr[0], np.max(h_arr), 8))
-
+fig, ax0 = plt.subplots(figsize=(8, 8))
+sns.lineplot(x=t_arr, y=thrust_arr, ax=ax0, color='black', linewidth=2)
+ax0.set_xlabel('Время, сек.')
+ax0.set_ylabel('Сила тяги, км.')
+ax0.set_title('График')
 
 # Create a figure with 2 subplots, arranged in a vertical layout
 fig, (ax2, ax3, ax4) = plt.subplots(nrows=3, ncols=1, figsize=(10,10), height_ratios=[2,1,1])
@@ -210,18 +215,15 @@ ax3.set_ylabel('Масса ЛА, кг.')
 ax3.set_title('График зависимости массы ЛА от времени')
 
 # Plot 4: Theta versus Time
-sns.lineplot(x=theta_arr, y=t_arr, ax=ax4, color='black', linewidth=1)
-ax4.set_xlabel('Угол Тета, град.')
-ax4.set_ylabel('Время, сек.')
-ax4.set_title('График зависимости угла Тета(наклона траектории полета) от времени')
-# Set the number of ticks on the x and y-axis
-ax4.set_xticks(np.linspace(np.min(theta_arr), np.max(theta_arr), 8))
+plt.scatter(theta_arr, t_arr, s=2, color='black')
+plt.xlabel('Угол Тета, град.')
+plt.ylabel('Время, сек.')
+plt.title('График зависимости угла Тета(наклона траектории полета) от времени')
+plt.xticks(np.linspace(np.min(theta_arr), np.max(theta_arr), 8))
 
 now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 ax4.text(0.5, -0.6, f"Данный график был сделан: {now}", ha='center', va='center', transform=ax4.transAxes)
-# Adjust the spacing between subplots
-plt.subplots_adjust(hspace=0.8)
+plt.subplots_adjust(hspace=0.8) # Adjust the spacing between subplots
 
-# Show the plot
 plt.show()
 # endregion
