ab5ef8e15a0ef0a9b2f3f63b37a3f79f25110bfa
Nicola <shirnin.kolia@gmail.com>
2023-04-19 20:58:51 +0300

    Add Drag + edit plots

--- main.py
+++ main.py
@@ -21,11 +21,6 @@ def ricocheting_trajectory(x, t):
     # TODO
     pass
 
-# The drag coefficient Cd is equal to the drag D divided by the quantity:
-# density r times half the velocity V squared times the reference area A.
-# Drag = CoeffcientDrag * rho * v^2 * (reference area / 2)
-def Drag(CoefficientDrag, rho, v, A):
-    return CoefficientDrag * rho * v**2 * (A / 2)
 
 # The lift coefficient Cl is equal to the lift L divided by the quantity:
 # density r times half the velocity V squared times the reference area A.
@@ -33,21 +28,6 @@ def Drag(CoefficientDrag, rho, v, A):
 def Lift(CoefficientLift, rho, v, L, d):
  return CoefficientLift * rho * v**2 * (L / 2)
 
-def density(h):
-    "Calculates air density at altitude"
-    rho0 = 1.225 #[kg/m^3] air density at sea level
-    if h < 19200:
-        #use barometric formula, where 8420 is effective height of atmosphere [m]
-        rho = rho0 * np.exp(-h/8420)
-    elif h > 19200 and h < 47000:
-        #use 1976 Standard Atmosphere model
-        #http://modelweb.gsfc.nasa.gov/atmos/us_standard.html
-        #from http://scipp.ucsc.edu/outreach/balloon/glost/environment3.html
-        rho = rho0 * (.857003 + h/57947)**-13.201
-    else:
-        #vacuum
-        rho = 1.e-6
-    return rho
 
 def temperature(h):
     "Calculates air temperature [Celsius] at altitude [m]"
@@ -91,13 +71,40 @@ m_ha = 1440 - m_fuel        # +initial mass of aircraft, kg
 m_total = m_ha + m_fuel     # total mass of aircraft, kg
 x = 0.0                     # initial Distance, m
 h = 2 * 1000.0              # 0-40.000 initial altitude, m
-v = 3 * 340.0               # 2-6 +initial velocity, m/s    1 mach = 340 m/s
+V = 3 * 340.0               # 2-6 +initial velocity, m/s    1 mach = 340 m/s
 alpha = np.radians(0)       # 0-10 +initial angle of attack, radians
 theta = np.radians(45)      # +initial angle of inclination of the flight trajectory, radians
 dt = 0.01                   # time step, s
 G_c = 0.02                  # +initial fuel burnout rate, N/s
 I_sp = 15.0                 # +initial specific impulse
 
+length = 12.5
+diameter = 0.5
+frontalArea = diameter**2/ (4 * np.pi)               # S = d**2 / (4 × π), где d — это диаметр
+CoefficientDrag = 0.4       # drag coefficient for cone 20 degrees
+
+def rhoDensity(h):
+    "Calculates air density at altitude"
+    rho0 = 1.225 #[kg/m^3] air density at sea level
+    if h < 19200:
+        #use barometric formula, where 8420 is effective height of atmosphere [m]
+        rho = rho0 * np.exp(-h/8420)
+    elif h > 19200 and h < 47000:
+        #use 1976 Standard Atmosphere model
+        #http://modelweb.gsfc.nasa.gov/atmos/us_standard.html
+        #from http://scipp.ucsc.edu/outreach/balloon/glost/environment3.html
+        rho = rho0 * (.857003 + h/57947)**-13.201
+    else:
+        #vacuum
+        rho = 1.e-6
+    return rho
+
+
+# The drag coefficient Cd is equal to the drag D divided by the quantity:
+# density r times half the velocity V squared times the reference area A.
+# Drag = CoeffcientDrag * rho * v^2 * (reference area / 2)
+def DragX(CoefficientDrag, h, v, frontalArea):
+    return CoefficientDrag * rhoDensity(h) * v**2 * (frontalArea / 2)
 
 def f(t):
     return G_c
@@ -108,13 +115,16 @@ def mass_after_fuel_burning(t, m_fuel):
     except spi.IntegrationWarning:
         return 0.0
 
-def Thrust(m_fuel, g, I_sp):
+def thrust(m_fuel, g, I_sp):
     return m_fuel * g * I_sp # kg *m/s != кг*м / c^2
 
-def hypersonic_aircraft_model(y, t, R_earth, g, T, alpha, m_total):
+def hypersonic_aircraft_model(y, t, R_earth, g, T, alpha, m_total, drag):
     V, theta, x, h = y
 
-    dV = (T * np.cos(alpha)) / m_total - g * np.sin(theta)
+    # dV = (T * np.cos(alpha)) / m_total - g * np.sin(theta)
+    # dtheta = (T * np.sin(alpha)) / (m_total * V) - (g * np.cos(theta)) / V + (V * np.cos(theta)) / (R_earth + h)
+
+    dV = (T * np.cos(alpha)) / m_total - drag / m_total - g * np.sin(theta)
     dtheta = (T * np.sin(alpha)) / (m_total * V) - (g * np.cos(theta)) / V + (V * np.cos(theta)) / (R_earth + h)
     dx = V * np.cos(theta) * R_earth / (R_earth + h)
     dy = V * np.sin(theta)
@@ -124,11 +134,11 @@ def hypersonic_aircraft_model(y, t, R_earth, g, T, alpha, m_total):
 
 # Integration interval
 t = 0.0                     # initial time
-y0 = [v, theta, x, h]       # initial conditions
+y0 = [V, theta, x, h]       # initial conditions
 
 # Arrays to store results
 t_arr = [0.0]
-v_arr = [v / 340.0]
+v_arr = [V / 340.0]
 theta_arr = [np.degrees(theta)]
 x_arr = [x / 1000]
 h_arr = [h / 1000]
@@ -139,13 +149,14 @@ while h > 0.0:
     if m_fuel > 0:
         m_fuel = mass_after_fuel_burning(t, m_fuel)
         m_total = m_ha + m_fuel
-        T = Thrust(m_fuel, g, I_sp)
+        T = thrust(m_fuel, g, I_sp)
     else:
         m_total = m_ha
         T = 0
 
+    drag = DragX(CoefficientDrag, h, V, frontalArea)
     # solve model
-    sol = odeint(hypersonic_aircraft_model, y0, [t, t + dt], args=(R_earth, g, T, alpha, m_total))
+    sol = odeint(hypersonic_aircraft_model, y0, [t, t + dt], args=(R_earth, g, T, alpha, m_total, drag))
 
     # Update y0
     y0 = sol[-1]
@@ -178,6 +189,11 @@ ax1.set_xlabel('Дальность, км.')
 ax1.set_ylabel('Высота, км.')
 ax1.set_title('График зависимости дальности и высоты ЛА')
 
+# Set the number of ticks on the x and y-axis
+ax1.set_xticks(np.linspace(x_arr[0], np.max(x_arr), 8))
+ax1.set_yticks(np.linspace(h_arr[0], np.max(h_arr), 8))
+
+
 # Create a figure with 2 subplots, arranged in a vertical layout
 fig, (ax2, ax3, ax4) = plt.subplots(nrows=3, ncols=1, figsize=(10,10), height_ratios=[2,1,1])
 
@@ -191,14 +207,15 @@ ax2.set_title('График зависимости скорости ЛА от в
 sns.lineplot(x=t_arr, y=m_arr, ax=ax3, color='black', linewidth=1)
 ax3.set_xlabel('Время, сек.')
 ax3.set_ylabel('Масса ЛА, кг.')
-ax3.set_title('График зависимости  массы ЛА от времени')
+ax3.set_title('График зависимости массы ЛА от времени')
 
 # Plot 4: Theta versus Time
 sns.lineplot(x=theta_arr, y=t_arr, ax=ax4, color='black', linewidth=1)
 ax4.set_xlabel('Угол Тета, град.')
 ax4.set_ylabel('Время, сек.')
 ax4.set_title('График зависимости угла Тета(наклона траектории полета) от времени')
-
+# Set the number of ticks on the x and y-axis
+ax4.set_xticks(np.linspace(np.min(theta_arr), np.max(theta_arr), 8))
 
 now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 ax4.text(0.5, -0.6, f"Данный график был сделан: {now}", ha='center', va='center', transform=ax4.transAxes)
