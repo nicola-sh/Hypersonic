aca855dbe0b058fb33631574a563f38c11754189
Nicola <shirnin.kolia@gmail.com>
2023-04-19 19:46:15 +0300

    Worked vers

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import datetime
 import warnings
 import numpy as np
-import scipy.integrate as spi
 import seaborn as sns
-from matplotlib import pyplot as plt, MatplotlibDeprecationWarning
+from matplotlib import pyplot as plt, ticker, MatplotlibDeprecationWarning
+import scipy.integrate as spi
 from scipy.integrate import odeint
 
 warnings.filterwarnings("ignore", category=MatplotlibDeprecationWarning)
@@ -20,15 +20,6 @@ def planning_trajectory(x, t):
 def ricocheting_trajectory(x, t):
     # TODO
     pass
-
-# ThrustEquation Thrust = dm/dt * g * I_sp
-# where:
-# dm/dt = mass flow rate
-# g = acceleration due to gravity
-# I_sp = specific impulse
-
-def ThrustEquation(t, m, g, I_sp):
-    return
 
 # The drag coefficient Cd is equal to the drag D divided by the quantity:
 # density r times half the velocity V squared times the reference area A.
@@ -95,15 +86,18 @@ R_earth = 6370000.0         # radius of the Earth, m
 
 # Initial condition of Hypersonic Aircraft
 T = 100000.0                # +initial thrust force, N
-m_ha = 1000.0               # +initial mass of aircraft, kg
 m_fuel = 450.0              # +initial mass fuel of aircraft, kg
+m_ha = 1440 - m_fuel        # +initial mass of aircraft, kg
+m_total = m_ha + m_fuel     # total mass of aircraft, kg
 x = 0.0                     # initial Distance, m
-h = 2 * 1000.0              # initial altitude, m
-v = 5 * 340.0               # +initial velocity, m/s    1 mach = 340 m/s
-alpha = np.radians(30)      # +initial angle of attack, radians
-theta = np.radians(0)       # +initial angle of inclination of the flight trajectory, radians
+h = 2 * 1000.0              # 0-40.000 initial altitude, m
+v = 3 * 340.0               # 2-6 +initial velocity, m/s    1 mach = 340 m/s
+alpha = np.radians(0)       # 0-10 +initial angle of attack, radians
+theta = np.radians(45)      # +initial angle of inclination of the flight trajectory, radians
 dt = 0.01                   # time step, s
-G_c = 0.02                  # +initial fuel burnout per dt
+G_c = 0.02                  # +initial fuel burnout rate, N/s
+I_sp = 15.0                 # +initial specific impulse
+
 
 def f(t):
     return G_c
@@ -114,6 +108,9 @@ def mass_after_fuel_burning(t, m_fuel):
     except spi.IntegrationWarning:
         return 0.0
 
+def Thrust(m_fuel, g, I_sp):
+    return m_fuel * g * I_sp # kg *m/s != кг*м / c^2
+
 def hypersonic_aircraft_model(y, t, R_earth, g, T, alpha, m_total):
     V, theta, x, h = y
 
@@ -123,25 +120,26 @@ def hypersonic_aircraft_model(y, t, R_earth, g, T, alpha, m_total):
     dy = V * np.sin(theta)
 
     return [dV, dtheta, dx, dy]
-# endregion
 
-# region Main program
+
 # Integration interval
 t = 0.0                     # initial time
 y0 = [v, theta, x, h]       # initial conditions
 
 # Arrays to store results
 t_arr = [0.0]
+v_arr = [v / 340.0]
+theta_arr = [np.degrees(theta)]
 x_arr = [x / 1000]
 h_arr = [h / 1000]
-v_arr = [v]
-m_arr = [m_ha + m_fuel]
+m_arr = [m_total]
 
 while h > 0.0:
 
     if m_fuel > 0:
         m_fuel = mass_after_fuel_burning(t, m_fuel)
         m_total = m_ha + m_fuel
+        T = Thrust(m_fuel, g, I_sp)
     else:
         m_total = m_ha
         T = 0
@@ -152,14 +150,16 @@ while h > 0.0:
     # Update y0
     y0 = sol[-1]
     V, theta, x, h = sol[-1]
+
     # Update time
     t += dt
 
     # Store results
     t_arr.append(t)
+    v_arr.append(V / 340)
+    theta_arr.append(np.degrees(theta))
     x_arr.append(x / 1000)
     h_arr.append(h / 1000)
-    v_arr.append(V)
     m_arr.append(m_total)
 
 
@@ -167,26 +167,43 @@ while h > 0.0:
 # Set the style for the plot
 sns.set_style("whitegrid")
 
+# Create a figure and axis object
+fig, ax1 = plt.subplots(figsize=(8, 8))
+
+# Plot the line
+sns.lineplot(x=x_arr, y=h_arr, ax=ax1, color='black', linewidth=2)
+
+# Set the axis labels and title
+ax1.set_xlabel('Дальность, км.')
+ax1.set_ylabel('Высота, км.')
+ax1.set_title('График зависимости дальности и высоты ЛА')
+
 # Create a figure with 2 subplots, arranged in a vertical layout
-fig, (ax1, ax2) = plt.subplots(nrows=2, ncols=1, figsize=(10, 10))
+fig, (ax2, ax3, ax4) = plt.subplots(nrows=3, ncols=1, figsize=(10,10), height_ratios=[2,1,1])
 
-# Plot 1: Altitude versus Range
-sns.lineplot(x=x_arr, y=h_arr, ax=ax1)
-ax1.set_xlabel('Range, km')
-ax1.set_ylabel('Height, km')
-ax1.set_title('Height vs Range')
+# Plot 2: Velocity on Time
+sns.lineplot(x=t_arr, y=v_arr, ax=ax2, color='black', linewidth=1)
+ax2.set_xlabel('Время, сек.')
+ax2.set_ylabel('Скорость ЛА, мах')
+ax2.set_title('График зависимости скорости ЛА от времени')
 
-# Plot 2 and 3: Speed versus Time and Dependence of Mass on Time
-sns.lineplot(x=t_arr, y=v_arr, ax=ax2, color='blue')
-sns.lineplot(x=t_arr, y=m_arr, ax=ax2, color='orange')
-ax2.set_xlabel('Time, s')
-ax2.set_ylabel('Speed, m/s / Mass, kg')
-ax2.set_title('Velocity and Mass by time')
+# Plot 3: Mass by Time
+sns.lineplot(x=t_arr, y=m_arr, ax=ax3, color='black', linewidth=1)
+ax3.set_xlabel('Время, сек.')
+ax3.set_ylabel('Масса ЛА, кг.')
+ax3.set_title('График зависимости  массы ЛА от времени')
+
+# Plot 4: Theta versus Time
+sns.lineplot(x=theta_arr, y=t_arr, ax=ax4, color='black', linewidth=1)
+ax4.set_xlabel('Угол Тета, град.')
+ax4.set_ylabel('Время, сек.')
+ax4.set_title('График зависимости угла Тета(наклона траектории полета) от времени')
+
 
 now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-ax2.text(0.5, -0.2, f"Local Time: {now}", ha='center', va='center', transform=ax2.transAxes)
+ax4.text(0.5, -0.6, f"Данный график был сделан: {now}", ha='center', va='center', transform=ax4.transAxes)
 # Adjust the spacing between subplots
-plt.subplots_adjust(hspace=0.5)
+plt.subplots_adjust(hspace=0.8)
 
 # Show the plot
 plt.show()
